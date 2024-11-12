268ab384fc700a85ccf03bf3766be26fb805e808
Sravani <22h51a05f6@cmrcet.ac.in>
2024-11-12 19:42:30 +0530

    initial changes

---
+++ prog1.java
@@ -0,0 +1,6 @@
+class prog1 {
+    public static void main(String[] args) {
+       System.out.print("sravani");
+       System.out.println("akhi");
+        }
+}
---
+++ prog2.java
@@ -0,0 +1,7 @@
+class prog2{
+public static void main(String[] args){
+int a=4;
+int b=5;
+System.out.println( a+b);
+}
+}
