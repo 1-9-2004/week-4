67e08b70a7ae560646020c95cfdc529397a4255a
Sravani <22h51a05f6@cmrcet.ac.in>
2024-11-19 19:46:44 +0530

    done by me

---
+++ just.java
@@ -0,0 +1,8 @@
+ class just {
+    public static void main(String[] args) {
+       int n=3;
+       int m=4;
+     int c=n*m;
+System.out.println(c);
+    }
+}
---
+++ welcome.js
@@ -0,0 +1,44 @@
+const http = require('http');
+const os = require('os');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+   if (req.url === '/details') {
+       res.writeHead(200, { 'Content-Type': 'text/html' });
+       const osInfo = `
+<h1>OS Information</h1>
+<p>Platform: ${os.platform()}</p>
+<p>Architecture: ${os.arch()}</p>
+<p>CPU Info: ${JSON.stringify(os.cpus())}</p>
+<p>Total Memory: ${os.totalmem()}</p>
+<p>Free Memory: ${os.freemem()}</p>
+   `;
+       const filePath = path.join(__dirname, 'files', 'example.txt');
+       const parsedPath = path.parse(filePath);
+       const pathInfo = `
+<h1>Path Information</h1>
+<p>File Path: ${filePath}</p>
+<p>Parsed Path: ${JSON.stringify(parsedPath)}</p>
+    `;
+        const combinedInfo = osInfo + pathInfo;
+        res.end(`
+ <!DOCTYPE html>
+ <html>
+ <head>
+ <title>System Details</title>
+ </head>
+ <body>
+        ${combinedInfo}
+ </body>
+ </html>
+    `);
+    } else {
+       res.writeHead(404, { 'Content-Type': 'text/plain' });
+       res.end('Page Not Found');
+    }
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+   console.log(`Server running on port ${PORT}`);
+});
